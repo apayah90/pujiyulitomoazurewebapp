feb6b6786405a1a29b61c11f92b95c80023e6874
apayah90 <apayah90@gmail.com>
2019-05-14 00:56:23 +0700

    Update config.php

--- config.php
+++ config.php
@@ -30,8 +30,9 @@ protected $link;
 // Check connection
      public function openConnection() {
      try {
-      $this->link = new PDO($this->server, $this->user,$this->pass,$this->options);
-      return $this->link;
+      $this->link = new PDO("sqlsrv:server = tcp:pujiyulitomowebappserver.database.windows.net,1433; Database = pujiyulitomowebapp", "apayah90", "terserah90");
+      $link->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+       return $this->link;
        echo "Berhasil terkoneksi ke database";
     } catch(Exception $e) {
         echo "Failed: " . $e->getMessage();
