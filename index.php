2ec69308bc6aaadf5b57239741d534fae0a1a307
apayah90 <apayah90@gmail.com>
2019-05-14 16:27:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
  <head>
  <Title>Registration Form</Title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
  <style type="text/css">
  	body { background-color: #fff; border-top: solid 10px #000;
  	    color: #333; font-size: .85em; margin: 20; padding: 20;
@@ -17,13 +18,14 @@
  </head>
  <body>
  <h1>Register here!</h1>
+  <a href="create.php" class="btn btn-success pull-right">Tambah Pembeli Baru</a>
  <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
  <form method="post" action="index.php" enctype="multipart/form-data" >
        Name  <input type="text" name="name" id="name"/></br></br>
        Email <input type="text" name="email" id="email"/></br></br>
        Job <input type="text" name="job" id="job"/></br></br>
        <input type="submit" name="submit" value="Submit" />
- <input type="submit" name="update" value="Update" />
+       <input type="submit" name="update" value="Update" />
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
