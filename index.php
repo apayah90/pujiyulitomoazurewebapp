41b7fc8e0abc732dff50790ae28b4c2073153bd0
apayah90 <apayah90@gmail.com>
2019-05-14 16:40:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -29,9 +29,17 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-  require 'config.php';
-        $database = new Config();
-        $conn = $database->openConnection();
+   $host = "pujiyulitomowebappserver.database.windows.net";
+    $user = "apayah90";
+    $pass = "terserah90!";
+    $db = "pujiyulitomowebapp";
+
+    try {
+        $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    } catch(Exception $e) {
+        echo "Failed: " . $e;
+    }
     if (isset($_POST['submit'])) {
         try {
             $name = $_POST['name'];
