9f9f6655488c939ba937154ab3b1de5fb5b9c839
apayah90 <apayah90@gmail.com>
2019-05-28 15:42:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,58 +11,18 @@
         echo "Failed: " . $e;
     }
  
-// Define variables and initialize with empty values
-$nama = $jenis = $bahan = $langkah = "";
-$nama_err = $jenis_err = $bahan_err = $langkah_err = "";
+
  
 // Processing form data when form is submitted
 if (isset($_POST['submit']))
 {
-    // Validate nama
-    $input_name = trim($_POST["nama"]);
-    if(empty($input_name)){
-        $name_err = "Masukan Nama ";
-    } elseif(!filter_var(trim($_POST["nama"]), FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z'-.\s ]+$/")))){
-        $nama_err = 'Silahkan masukan nama yang valid';
-    } else{
-        $nama = $input_name;
-    }
-    
-    // Validate email
-    $input_jenis = trim($_POST["jenis"]);
-    if(empty($input_jenis)){
-        $jenis_err = 'Silahkan masukan jenis masakan : Makanan / Sayuran / Buah buahan';     
-    } else{
-        $jenis = $input_jenis;
-    }
-    
-    // Validate poin
-    $input_bahan = trim($_POST["bahan"]);
-    if(empty($input_bahan)){
-        $bahan_err = "Silahkan Masukan bahan";     
-    } 
-    else{
-        $bahan = $input_bahan;
-    }
-	
-	// Validate badge
-	$input_langkah = trim($_POST["langkah"]);
-	if(empty($input_langkah)) {
-		$telp_err = "Silahkan Masukan Langkah ";
-    }
-    
-		else {
-            $langkah = $input_langkah;
-            
-		}
-	
-    
-    // Check input errors before inserting in database
-    if(empty($nama_err) && empty($jenis_err) && empty($bahan_err) && empty($langkah_err)){
-     
+
        try {
-        $database = new Config();
-        $dba = $database->openConnection();
+            $nama = $_POST['nama'];
+            $jenis = $_POST['jenis'];
+            $bahan = $_POST['bahan'];
+            $langkah = $_POST['langkah'];
+
 
         //Prepare an insert statement
  
@@ -70,10 +30,6 @@ if (isset($_POST['submit']))
             $sql_insert = "INSERT INTO Resep (nama, jenis, bahan, langkah) 
                         VALUES (?,?,?,?)";
 
-         $nama = $_POST['nama'];
-            $jenis = $_POST['jenis'];
-            $bahan = $_POST['bahan'];
-            $langkah = $_POST['langkah'];
 
             $stmt = $conn->prepare($sql_insert);
             $stmt->bindParam(1, $nama);
@@ -89,12 +45,13 @@ if (isset($_POST['submit']))
        }
         echo "<h3>Your're registered!</h3>";
 
-    }
+    
     
 
    
 }
 ?>
+
 
  
 <!DOCTYPE html>
@@ -120,22 +77,22 @@ if (isset($_POST['submit']))
                     </div>
                     <p>Please fill this form and submit to add recipe record to the database.</p>
                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                        <div class="form-group <?php echo (!empty($nama_err)) ? 'has-error' : ''; ?>">
+                        <div class="form-group">
                             <label>Nama</label>
                             <input type="text" name="nama" class="form-control" value="<?php echo $nama; ?>">
                             <span class="help-block"><?php echo $nama_err;?></span>
                         </div>
-                        <div class="form-group <?php echo (!empty($jenis_err)) ? 'has-error' : ''; ?>">
+                        <div class="form-group">
                             <label>Jenis</label>
                             <textarea name="jenis" class="form-control"><?php echo $jenis; ?></textarea>
                             <span class="help-block"><?php echo $jenis_err;?></span>
                         </div>
-                        <div class="form-group <?php echo (!empty($bahan_err)) ? 'has-error' : ''; ?>">
+                        <div class="form-group">
                             <label>Bahan</label>
                             <input type="text" name="bahan" class="form-control" value="<?php echo $bahan; ?>">
                             <span class="help-block"><?php echo $bahan_err;?></span>
                         </div>
-						<div class="form-group <?php echo (!empty($langkah_err)) ? 'has-error' : ''; ?>">
+			<div class="form-group">
                             <label>Langkah</label>
                             <textarea name="langkah" class="form-control"><?php echo $langkah; ?></textarea>
                             <span class="help-block"><?php echo $langkah_err;?></span>
