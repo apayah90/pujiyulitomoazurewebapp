a049c2359d95c95e77bb33080894018a4bbc861c
apayah90 <apayah90@gmail.com>
2019-05-31 13:18:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,113 +1,12 @@
+<?php
+require_once 'vendor/autoload.php';
 
 
-
- 
-<!DOCTYPE html>
-<html lang="en">
-	<?php include "head.php";?>
-<head>
-    <meta charset="UTF-8">
-    <title>Create Recipe</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-
-    <style type="text/css">
-        .wrapper{
-            width: 500px;
-            margin: 0 auto;
-        }
-    </style>
-
-</head>
-<body>
-	
-		<?php include "header.php"; ?>
-	<!-- start: Page Title -->
-	<div id="page-title">
-
-		<div id="page-title-inner">
-
-			<!-- start: Container -->
-			<div class="container">
-
-				<h2>Menu Makanan Buka Puasa</h2>
-
-			</div>
-			<!-- end: Container  -->
-
-		</div>	
-
-	</div>
-	
-	<!-- Start Wrapper -->
-    <div class="wrapper">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="page-header">
-                        <h2>Create Record</h2>
-                    </div>
-                    <p>Please fill this form and submit to add recipe record to the database.</p>
-                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                        
-
-			    
-			<div class="form-group">
-                            <label>Upload</label>
-				
-				<input type="file" name="fileToUpload" accept=".jpeg,.jpg,.png" required="">
-			
-			    
-                        </div>
-			    
-			<div class="form-group">
-                            <label>Nama</label>
-				
-                            <textarea type="text" name="nama" class="form-control" value="<?php echo $nama; ?>"></textarea>
-                            <span class="help-block"><?php echo $nama_err;?></span>
-			    
-                        </div>
-			    
-                        <div class="form-group">
-                            <label>Jenis</label>
-                            <textarea name="jenis" class="form-control"><?php echo $jenis; ?></textarea>
-                            <span class="help-block"><?php echo $jenis_err;?></span>
-                        </div>
-                        <div class="form-group">
-                            <label>Bahan</label>
-                            <textarea type="text" name="bahan" class="form-control" value="<?php echo $bahan; ?>"></textarea>
-                            <span class="help-block"><?php echo $bahan_err;?></span>
-                        </div>
-			<div class="form-group">
-                            <label>Langkah</label>
-                            <textarea name="langkah" class="form-control"><?php echo $langkah; ?></textarea>
-                            <span class="help-block"><?php echo $langkah_err;?></span>
-                        </div>
-			    
-			 <div class="form-group">
-                            <label>Keterangan</label>
-                
-                            <textarea type="text" name="keterangan" class="form-control" value="<?php echo $keterangan; ?>"></textarea>
-                            <span class="help-block"><?php echo $bahan_err;?></span>
-                
-                        </div>
-                        <input type="submit" name="submit" class="btn btn-primary" value="Submit">
-			    <input type="submit" name="submit2" class="btn btn-primary" value="Upload">
-                        <a href="menu.php" class="btn btn-default">Produk</a>
-                    </form>
-                </div>
-            </div>        
-        </div>
-    </div>
-	<?php
-	
-	require_once 'vendor/autoload.php';
-	
 use MicrosoftAzure\Storage\Blob\BlobRestProxy;
 use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
 use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
 use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
 use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
-	
 
 // This config file
    $host = "pujiyulitomowebappserver.database.windows.net";
@@ -119,56 +18,15 @@ use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
         $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
     } catch(Exception $e) {
         echo "Failed: " . $e;
-    }
- 
-
- 
-// Processing form data when form is submitted
-if (isset($_POST['submit']))
-{
-
-	//sql
-       try {
-            $nama = $_POST['nama'];
-            $jenis = $_POST['jenis'];
-            $bahan = $_POST['bahan'];
-            $langkah = $_POST['langkah'];
-	    $keterangan = $_POST['keterangan'];
+    } 
 
 
-        //Prepare an insert statement
- 
-        // Insert data
-            $sql_insert = "INSERT INTO Resep (nama, jenis, bahan, langkah, keterangan) 
-                        VALUES (?,?,?,?,?)";
 
-
-            $stmt = $conn->prepare($sql_insert);
-            $stmt->bindParam(1, $nama);
-            $stmt->bindParam(2, $jenis);
-            $stmt->bindParam(3, $bahan);
-            $stmt->bindParam(4, $langkah);
-	    $stmt->bindParam(5, $keterangan);
-
-            $stmt->execute();
-
-           
-       } catch (Exception $e) {
-           echo "Failed". $e;
-       }
-        echo "<h3>Your're registered!</h3>";
-
-    
-    
-
-   
-}
-//Upload blob	
 $connectionString = "DefaultEndpointsProtocol=https;AccountName=apayahstorage;AccountKey=l5SpvHYLpKnyEZgyGKA1vuMmmL18jAvZFxGBZPyPxcUB7s0e10yaqSDVauos596TmhjUYH4chpMGUxXvIpK1TA==;";
 $containerName = "blockblobsiuqbmh";
 // Create blob client.
 $blobClient = BlobRestProxy::createBlobService($connectionString);
-if (isset($_POST['submit2'])) {
+if (isset($_POST['submit'])) {
     $fileToUpload = strtolower($_FILES["fileToUpload"]["name"]);
     $content = fopen($_FILES["fileToUpload"]["tmp_name"], "r");
     
@@ -189,10 +47,111 @@ if (isset($_POST['submit2'])) {
             $listBlobsOptions->setContinuationToken($result->getContinuationToken());
         } while($result->getContinuationToken());
         echo "<br />";
+        // Insert data
 
+        $gambar = $var;
+            $sql_insert = "INSERT INTO Resep (gambar) 
+                        VALUES (?)";
+            $stmt = $conn->prepare($sql_insert);
+            $stmt->bindParam(1, $gambar);
+
+            $stmt->execute();
  }
 
 echo $var;
+
+
+     
+
 ?>
-</body>
+
+<!DOCTYPE html>
+<html>
+ <head>
+ <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="https://raw.githubusercontent.com/muhrizky/Smart-Parkir/master/parking_meter__2__Mrq_icon.ico">
+
+    <title>Undip Smart Parkir</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/starter-template/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="starter-template.css" rel="stylesheet">
+  </head>
+<body>
+	<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarsExampleDefault">
+			<ul class="navbar-nav mr-auto">
+			<li class="nav-item">
+				<a class="nav-link" href="https://smart-parkir.azurewebsites.net/">Home</a>
+			</li>
+			<li class="nav-item active">
+				<a class="nav-link" href="https://smart-parkir.azurewebsites.net/analyze.php">Analisis Kendaraan<span class="sr-only">(current)</span></a>
+			</li>
+		</div>
+		</nav>
+		<main role="main" class="container">
+    		<div class="starter-template"> <br><br><br>
+        		<h1>Analisis Kendaraan</h1>
+				<p class="lead">Pilih Foto Kendaraan Anda.<br> Kemudian Click <b>Upload</b>, untuk menganlisa foto pilih <b>analyze</b> pada tabel.</p>
+				<span class="border-top my-3"></span>
+			</div>
+		<div class="mt-4 mb-2">
+			<form class="d-flex justify-content-lefr" action="analyze.php" method="post" enctype="multipart/form-data">
+				<input type="file" name="fileToUpload" accept=".jpeg,.jpg,.png" required="">
+				<input type="submit" name="submit" value="Upload">
+			</form>
+		</div>
+		<br>
+		<br>
+		<h4>Total Files : <?php echo sizeof($result->getBlobs())?></h4>
+		<table class='table table-hover'>
+			<thead>
+				<tr>
+					<th>File Name</th>
+					<th>File URL</th>
+					<th>Action</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php
+				do {
+					foreach ($result->getBlobs() as $blob)
+					{
+						?>
+						<tr>
+							<td><?php echo $blob->getName() ?></td>
+							<td><?php echo $blob->getUrl() ?></td>
+							<td>
+								<form action="computervision.php" method="post">
+									<input type="hidden" name="url" value="<?php echo $blob->getUrl()?>">
+									<input type="submit" name="submit" value="Analyze!" class="btn btn-primary">
+								</form>
+							</td>
+						</tr>
+						<?php
+					}
+					$listBlobsOptions->setContinuationToken($result->getContinuationToken());
+				} while($result->getContinuationToken());
+				?>
+			</tbody>
+		</table>
+
+	</div>
+
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="https://getbootstrap.com/docs/4.0/assets/js/vendor/popper.min.js"></script>
+    <script src="https://getbootstrap.com/docs/4.0/dist/js/bootstrap.min.js"></script>
+  </body>
 </html>
