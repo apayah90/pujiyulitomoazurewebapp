dbacc211223ce3a24fe7ad52e24606dff02d7cd2
apayah90 <apayah90@gmail.com>
2019-06-02 14:07:20 +0700

    Update tesmenu.php

--- tesmenu.php
+++ tesmenu.php
@@ -17,38 +17,39 @@
 <?php include "head.php";?>
 <body>
     
-	<?php include "header.php"; ?>
-	<!-- start: Page Title -->
-	<div id="page-title">
+    <?php include "header.php"; ?>
+    <!-- start: Page Title -->
+    <div id="page-title">
 
-		<div id="page-title-inner">
+        <div id="page-title-inner">
 
-			<!-- start: Container -->
-			<div class="container">
+            <!-- start: Container -->
+            <div class="container">
 
-				<h2>Menu Makanan Buka Puasa</h2>
+                <h2>Menu Makanan Buka Puasa</h2>
 
-			</div>
-			<!-- end: Container  -->
+            </div>
+            <!-- end: Container  -->
 
-		</div>	
+        </div>  
 
-	</div>
-	<!-- end: Page Title -->
-	
-	<!--start: Wrapper-->
-	<div id="wrapper">
-				
-		<!--start: Container -->
-    	<div class="container"> 
+    </div>
+    <!-- end: Page Title -->
+    
+    <!--start: Wrapper-->
+    <div id="wrapper">
+                
+        <!--start: Container -->
+        <div class="container"> 
         <!--<div class="title"><h3>Keranjang Anda</h3></div>
             <div class="hero-unit">
             </div> -->            
-      		<!-- start: Row -->
+            <!-- start: Row -->
             
-      		<div class="row">
-			<?php
-try {
+            <div class="row">
+            <?php
+            
+            try {
             $sql_select = "SELECT * FROM Registration";
             $stmt = $conn->query($sql_select);
             $registrants = $stmt->fetchAll(); 
@@ -74,69 +75,79 @@ try {
         }
        
 ?>
-        		<div class="span4">
-          			<div class="icons-box">
-                        <div class="title"><h3>"Nama <?php echo $registrant['email'];?>"</h3></div>
-                        <img src="" style="border: 2px solid grey; border-radius: 5px; width: 250px; height: 200px;"/>
-						<div><h3>Rp.</h3></div>
-					<!--	<p>
-						
-						</p> -->
-						<div class="clear"><a href="detailproduk.php?hal=detailbarang&kd=<?php echo $data['kode'];?>" class="btn btn-lg btn-danger">Detail</a>  <a href="index.html" class="btn btn-lg btn-success">Beli &raquo;</a></div>
+            <!-- start: Row -->
+            
+            <div class="row">
+                    <?php
+                    $sql_select = "SELECT * FROM Resep";
+                    $stmt = $conn->query($sql_select);
+                    $datas = $stmt->fetchAll();
+                    foreach($datas as $data) { 
 
+                    ?>
+                <div class="span4">
+                    <div class="icons-box">
+                        <div class="title"><h3><?php echo $data['nama']; ?></h3></div>
+                        <img src="<?php echo $data['gambar']; ?>" style="border: 2px solid grey; border-radius: 5px; width: 250px; height: 200px;"  />
+                        <div><h3>Rp.<?php echo $data['keterangan']; ?></h3></div>
+                    <!--    <p>
+                        
+                        </p> -->
+                        <div class="clear"><a href="detailproduk.php?kd=<?php echo $data['kode'];?>" class="btn btn-lg btn-danger">Detail</a> <a href="detailproduk.php?kd=<?php echo $data['kode'];?>" class="clear"> <a href="index.html" class="btn btn-lg btn-success">Beli &raquo;</a></div>
+                    
                     </div>
-        		</div>
+                </div>
                 <?php   
-  
+                    }
               
               ?>
 <!---->
-      		</div>
-			<!-- end: Row -->
-					
-					
-				</div>	
-				
-					
-				</div>
-				
-			</div>
-			<!--end: Row-->
-	
-		</div>
-		<!--end: Container-->
-				
-		<!--start: Container -->
-    	<div class="container">	
-      		
-			<hr>
-		
-			<!-- start Clients List	
-			<div class="clients-carousel">
-		
-				<ul class="slides clients">
-					<li><img src="img/logos/1.png" alt=""/></li>
-					<li><img src="img/logos/2.png" alt=""/></li>	
-					<li><img src="img/logos/3.png" alt=""/></li>
-					<li><img src="img/logos/4.png" alt=""/></li>
-					<li><img src="img/logos/5.png" alt=""/></li>
-					<li><img src="img/logos/6.png" alt=""/></li>
-					<li><img src="img/logos/7.png" alt=""/></li>
-					<li><img src="img/logos/8.png" alt=""/></li>
-					<li><img src="img/logos/9.png" alt=""/></li>
-					<li><img src="img/logos/10.png" alt=""/></li>		
-				</ul>
-		
-			</div>
-			end Clients List -->
-		
-		
-		
-		</div>
-		<!--end: Container-->	
+            </div>
+            <!-- end: Row -->
+                    
+                    
+                </div>  
+                
+                    
+                </div>
+                
+            </div>
+            <!--end: Row-->
+    
+        </div>
+        <!--end: Container-->
+                
+        <!--start: Container -->
+        <div class="container"> 
+            
+            <hr>
+        
+            <!-- start Clients List 
+            <div class="clients-carousel">
+        
+                <ul class="slides clients">
+                    <li><img src="img/logos/1.png" alt=""/></li>
+                    <li><img src="img/logos/2.png" alt=""/></li>    
+                    <li><img src="img/logos/3.png" alt=""/></li>
+                    <li><img src="img/logos/4.png" alt=""/></li>
+                    <li><img src="img/logos/5.png" alt=""/></li>
+                    <li><img src="img/logos/6.png" alt=""/></li>
+                    <li><img src="img/logos/7.png" alt=""/></li>
+                    <li><img src="img/logos/8.png" alt=""/></li>
+                    <li><img src="img/logos/9.png" alt=""/></li>
+                    <li><img src="img/logos/10.png" alt=""/></li>       
+                </ul>
+        
+            </div>
+            end Clients List -->
+        
+        
+        
+        </div>
+        <!--end: Container-->   
 
-	</div>
-	<!-- end: Wrapper  -->			
+    </div>
+    <!-- end: Wrapper  -->          
 
 <!-- start: Java Script -->
 <!-- Placed at the end of the document so the pages load faster -->
@@ -150,4 +161,4 @@ try {
 <!-- end: Java Script -->
 
 </body>
-</html>	
+</html> 
