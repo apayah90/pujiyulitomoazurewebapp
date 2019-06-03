a2f6073f068b77a15acc0cbc32f84983ad73e0a8
apayah90 <apayah90@gmail.com>
2019-06-03 09:57:13 +0700

    Update tesmenu.php

--- tesmenu.php
+++ tesmenu.php
@@ -47,34 +47,6 @@
             <!-- start: Row -->
             
             <div class="row">
-            <?php
-            
-            try {
-            $sql_select = "SELECT * FROM Registration";
-            $stmt = $conn->query($sql_select);
-            $registrants = $stmt->fetchAll(); 
-            if(count($registrants) > 0) {
-                echo "<h2>People who are registered:</h2>";
-                echo "<table>";
-                echo "<tr><th>Name</th>";
-                echo "<th>Email</th>";
-                echo "<th>Job</th>";
-                echo "<th>Date</th></tr>";
-                foreach($registrants as $registrant) {
-                    echo "<tr><td>".$registrant['name']."</td>";
-                    echo "<td>".$registrant['email']."</td>";
-                    echo "<td>".$registrant['job']."</td>";
-                    echo "<td>".$registrant['date']."</td></tr>";
-                }
-                echo "</table>";
-            } else {
-                echo "<h3>No one is currently registered.</h3>";
-            }
-        } catch(Exception $e) {
-            echo "Failed: " . $e;
-        }
-       
-?>
             <!-- start: Row -->
             
             <div class="row">
